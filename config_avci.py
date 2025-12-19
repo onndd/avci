4f5ab675c8827898e1a1e5dd9573970f662d215d
onndd <odsnmn27@gmail.com>
2025-12-19 20:41:52 +0300

    Config: Stricter penalty for 3x/5x

--- config_avci.py
+++ config_avci.py
@@ -11,10 +11,10 @@ WINDOWS = [15, 25, 50, 100, 200, 300]
 # SCORING_1_5 Removed.
 
 SCORING_2_0 = {'TP': 135, 'TN': 10, 'FP': 140, 'FN': 35, 'PRECISION': 80}
-SCORING_3_0 = {'TP': 400, 'TN': 10, 'FP': 185, 'FN': 50, 'PRECISION': 100}
+SCORING_3_0 = {'TP': 400, 'TN': 10, 'FP': 300, 'FN': 50, 'PRECISION': 100}
 
 # High Multipliers - Aggressive Reward but Strict Precision Bonus
-SCORING_5_0 = {'TP': 700, 'TN': 25, 'FP': 200, 'FN': 65, 'PRECISION': 150}
+SCORING_5_0 = {'TP': 700, 'TN': 25, 'FP': 350, 'FN': 65, 'PRECISION': 150}
 SCORING_10_0 = {'TP': 1850, 'TN': 1, 'FP': 350, 'FN': 50, 'PRECISION': 200}
 SCORING_20_0 = {'TP': 5000, 'TN': 1, 'FP': 300, 'FN': 50, 'PRECISION': 300}
 SCORING_30_0 = {'TP': 8000, 'TN': 1, 'FP': 450, 'FN': 50, 'PRECISION': 350}  # New
